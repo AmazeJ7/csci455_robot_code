a221c272c15006e243a306b377701cb3fa692409
Johnny Gaddis <johnnygaddis777@gmail.com>
2018-02-27 03:41:52 -0700

    what am I doing up

--- CSCI455_Assignment_2.py
+++ CSCI455_Assignment_2.py
@@ -1,198 +1,180 @@
 from Maestro import Controller
 from tkinter import *
-import os
 import time
+import os
 
-# x = Controller()
-#
-# for chan in range(len(x.Targets)):
-#      x.setTarget(chan, 6000)
-#
-# x.setAccel(0, 10)
-# x.setAccel(1, 10)
-# x.setAccel(2, 30)
-# x.setAccel(3, 20)
-# x.setAccel(3, 20)
+# Maestro instantiation
+# controller = Controller()
+# for chan in range(len(controller.Targets)):
+#      controller.setTarget(chan, 6000)
+# controller.setAccel(0, 10)
+# controller.setAccel(1, 10)
+# controller.setAccel(2, 30)
+# controller.setAccel(3, 20)
+# controller.setAccel(3, 20)
+# os.system('xset r off')
 
-os.system('xset r off')
-
+# Global Variables
 xpos = 0
-ypos = 300
+ypos = 0
 actions = []
 pic_size = 100
 
+# Main Tk window instantiation
 root = Tk()
+root.title("Johnny and Brett do a Robot Control GUI")
 
-ht_icon = PhotoImage(file="icons\HeadTilt.png")
-ht_icon = ht_icon.subsample(2, 2)
-hr_icon = PhotoImage(file="icons\HeadRotate.png")
-hr_icon = hr_icon.subsample(2, 2)
-run_icon = PhotoImage(file="icons\Move.png")
-run_icon = run_icon.subsample(2, 2)
-turn_icon = PhotoImage(file="icons\Rotate.png")
-turn_icon = turn_icon.subsample(2, 2)
-br_icon = PhotoImage(file="icons\BodyRotate.png")
-br_icon = br_icon.subsample(2, 2)
-wait_icon = PhotoImage(file="icons\Wait.png")
-wait_icon = wait_icon.subsample(2, 2)
+# Icon files and subsample
+icons = [PhotoImage(file="icons\HeadTilt.png"), PhotoImage(file="icons\HeadRotate.png"),
+         PhotoImage(file="icons\Move.png"), PhotoImage(file="icons\Rotate.png"),
+         PhotoImage(file="icons\BodyRotate.png"), PhotoImage(file="icons\Wait.png")]
+for i in range(len(icons)):
+    icons[i] = icons[i].subsample(2, 2)
 
 
-def get_icon(name):
-    if name == "ht_icon":
-        return ht_icon
-    elif name == "hr_icon":
-        return hr_icon
-    elif name == "run_icon":
-        return run_icon
-    elif name == "turn_icon":
-        return turn_icon
-    elif name == "br_icon":
-        return br_icon
-    elif name == "wait_icon":
-        return wait_icon
-
-
+# Function to run all actions
 def go():
     print('go!')
-    # for x in range(len(actions)):
-    #     if actions[x].name == 'ht':
-    #         if actions[x].pos == 1:
-    #             x.setTarget(4, 4000)
-    #         elif actions[x].pos == 2:
-    #             x.setTarget(4, 5000)
-    #         elif actions[x].pos == 3:
-    #             x.setTarget(4, 7000)
-    #         elif actions[x].pos == 4:
-    #             x.setTarget(4, 8000)
-    #         elif actions[x].pos == 0:
-    #             x.setTarget(4, 6000)
-    #         time.sleep(actions[x].time)
-    #         x.setTarget(4, 6000)
-    #     elif actions[x].name == 'hr':
-    #         if actions[x].pos == 1:
-    #             x.setTarget(3, 4000)
-    #         elif actions[x].pos == 2:
-    #             x.setTarget(3, 5000)
-    #         elif actions[x].pos == 3:
-    #             x.setTarget(3, 7000)
-    #         elif actions[x].pos == 4:
-    #             x.setTarget(3, 8000)
-    #         elif actions[x].pos == 0:
-    #             x.setTarget(3, 6000)
-    #         time.sleep(actions[x].time)
-    #         x.setTarget(3, 6000)
-    #     elif actions[x].name == 'run':
-    #         if actions[x].pos == 1:
-    #             x.setTarget(1, 5000)
-    #         elif actions[x].pos == 2:
-    #             x.setTarget(1, 7000)
-    #         elif actions[x].pos == 0:
-    #             x.setTarget(1, 6000)
-    #         time.sleep(actions[x].time)
-    #         x.setTarget(1, 6000)
-    #     elif actions[x].name == 'turn':
-    #         if actions[x].pos == 1:
-    #             x.setTarget(2, 7000)
-    #         elif actions[x].pos == 2:
-    #             x.setTarget(2, 5000)
-    #         elif actions[x].pos == 0:
-    #             x.setTarget(2, 6000)
-    #         time.sleep(actions[x].time)
-    #         x.setTarget(2, 6000)
-    #     elif actions[x].name == 'br':
-    #         if actions[x].pos == 1:
-    #             x.setTarget(0, 4250)
-    #         elif actions[x].pos == 2:
-    #             x.setTarget(0, 7750)
-    #         elif actions[x].pos == 0:
-    #             x.setTarget(0, 6000)
-    #         time.sleep(actions[x].time)
-    #         x.setTarget(0, 6000)
-    #     elif actions[x].name == 'wait':
-    #         time.sleep(actions[x].time)
+    for x in range(len(actions)):
+        if actions[x].name == 'Head Tilt':
+            # if actions[x].pos == 1:
+            #     controller.setTarget(4, 4000)
+            # elif actions[x].pos == 2:
+            #     controller.setTarget(4, 5000)
+            # elif actions[x].pos == 3:
+            #     controller.setTarget(4, 7000)
+            # elif actions[x].pos == 4:
+            #     controller.setTarget(4, 8000)
+            # elif actions[x].pos == 0:
+            #     controller.setTarget(4, 6000)
+            time.sleep(actions[x].time)
+            # controller.setTarget(4, 6000)
+        elif actions[x].name == 'Head Rotate':
+            # if actions[x].pos == 1:
+            #     controller.setTarget(3, 4000)
+            # elif actions[x].pos == 2:
+            #     controller.setTarget(3, 5000)
+            # elif actions[x].pos == 3:
+            #     controller.setTarget(3, 7000)
+            # elif actions[x].pos == 4:
+            #     controller.setTarget(3, 8000)
+            # elif actions[x].pos == 0:
+            #     controller.setTarget(3, 6000)
+            time.sleep(actions[x].time)
+            # controller.setTarget(3, 6000)
+        elif actions[x].name == 'Move':
+            # if actions[x].pos == 1:
+            #     controller.setTarget(1, 5000)
+            # elif actions[x].pos == 2:
+            #     controller.setTarget(1, 7000)
+            # elif actions[x].pos == 0:
+            #     controller.setTarget(1, 6000)
+            time.sleep(actions[x].time)
+            # controller.setTarget(1, 6000)
+        elif actions[x].name == 'Turn':
+            # if actions[x].pos == 1:
+            #     controller.setTarget(2, 7000)
+            # elif actions[x].pos == 2:
+            #     controller.setTarget(2, 5000)
+            # elif actions[x].pos == 0:
+            #     controller.setTarget(2, 6000)
+            time.sleep(actions[x].time)
+            # controller.setTarget(2, 6000)
+        elif actions[x].name == 'Body Rotate':
+            # if actions[x].pos == 1:
+            #     controller.setTarget(0, 4250)
+            # elif actions[x].pos == 2:
+            #     controller.setTarget(0, 7750)
+            # elif actions[x].pos == 0:
+            #     controller.setTarget(0, 6000)
+            time.sleep(actions[x].time)
+            # controller.setTarget(0, 6000)
+        elif actions[x].name == 'Wait':
+            time.sleep(actions[x].time)
 
 
-def ht():
-    actions.append(Action('ht'))
-
-
-def hr():
-    actions.append(Action('hr'))
-
-
-def run():
-    actions.append(Action('run'))
-
-
-def turn():
-    actions.append(Action('turn'))
-
-
-def br():
-    actions.append(Action('br'))
-
-
-def wait():
-    actions.append(Action('wait'))
-
-
+# Function to delete all actions and reset canvas
 def del_all():
-    global xpos, actions
+    global xpos, ypos, actions
     canvas.delete("all")
     xpos = 0
+    ypos = 0
     actions = []
 
 
+# Class to represent an action to be run
 class Action:
-    def __init__(self, name):
-        global xpos
+    def __init__(self, name, icon):
+        global xpos, ypos
         self.name = name
-        self.canvas = canvas.create_image(50 + xpos, ypos, image=(get_icon(self.name + "_icon")))
+        self.icon = icon
+        if xpos < 800:
+            canvas.create_image(50 + xpos, 50 + ypos, image=self.icon)
+        else:
+            ypos += 105
+            xpos = 0
+            canvas.create_image(50 + xpos, 50 + ypos, image=self.icon)
         self.time = 2
         self.pos = 0
-        self.tk = ''
+        self.settings_tk = ''
+        self.animate_tk = ''
         xpos += 105
 
+    def animate(self):
+        self.animate_tk = Tk()
+        l = Label(self.animate_tk, text=self.name)
+        l.pack()
+        can = Canvas(self.animate_tk, bg="#1F1F1F", width=105 * self.time - 5, height=100)
+        can.pack()
+        for x in range(self.time):
+            can.update()
+            can.create_rectangle(0 + 105 * x, 0, 100 + 105 * x, 100, fill="red")
+            time.sleep(1)
+        self.animate_tk.destroy()
+        self.animate_tk.mainloop()
+
+    # Function to edit settings or remove instance
     def open_settings(self):
         def save_val():
             self.time = time_scale.get()
             self.pos = position.get()
+            self.settings_tk.destroy()
 
-        self.tk = Tk()
-        label1 = Label(self.tk, text="Time (S)")
+        self.settings_tk = Tk()
+        label1 = Label(self.settings_tk, text="Time (S)")
         label1.pack()
-        time_scale = Scale(self.tk, from_=1, to=10, orient=HORIZONTAL)
+        time_scale = Scale(self.settings_tk, from_=1, to=10, orient=HORIZONTAL)
         time_scale.pack()
-        if self.name == 'ht':
-            label1 = Label(self.tk, text="Position (Left to Right)")
+        if self.name == 'Head Tilt':
+            label1 = Label(self.settings_tk, text="Position (Left to Right)")
             label1.pack()
-            position = Scale(self.tk, from_=1, to=4, orient=HORIZONTAL)
+            position = Scale(self.settings_tk, from_=1, to=4, orient=HORIZONTAL)
             position.pack()
-        elif self.name == 'hr':
-            label1 = Label(self.tk, text="Position (Left to Right)")
+        elif self.name == 'Head Rotate':
+            label1 = Label(self.settings_tk, text="Position (Left to Right)")
             label1.pack()
-            position = Scale(self.tk, from_=1, to=4, orient=HORIZONTAL)
+            position = Scale(self.settings_tk, from_=1, to=4, orient=HORIZONTAL)
             position.pack()
-        elif self.name == 'turn':
-            label1 = Label(self.tk, text="Direction (Left or Right)")
+        elif self.name == 'Turn':
+            label1 = Label(self.settings_tk, text="Direction (Left or Right)")
             label1.pack()
-            position = Scale(self.tk, from_=1, to=2, orient=HORIZONTAL)
+            position = Scale(self.settings_tk, from_=1, to=2, orient=HORIZONTAL)
             position.pack()
-        elif self.name == 'run':
-            label1 = Label(self.tk, text="Direction (Foreword or Backward)")
+        elif self.name == 'Move':
+            label1 = Label(self.settings_tk, text="Direction (Foreword or Backward)")
             label1.pack()
-            position = Scale(self.tk, from_=1, to=2, orient=HORIZONTAL)
+            position = Scale(self.settings_tk, from_=1, to=2, orient=HORIZONTAL)
             position.pack()
-        elif self.name == 'br':
-            label1 = Label(self.tk, text="Position (Left to Right)")
+        elif self.name == 'Body Rotate':
+            label1 = Label(self.settings_tk, text="Position (Left to Right)")
             label1.pack()
-            position = Scale(self.tk, from_=1, to=4, orient=HORIZONTAL)
+            position = Scale(self.settings_tk, from_=1, to=4, orient=HORIZONTAL)
             position.pack()
-        save_values = Button(self.tk, text="Save Values", command=save_val)
+        save_values = Button(self.settings_tk, text="Save Values", command=save_val)
         save_values.pack()
-        delete = Button(self.tk, text="Delete", command=self.__delete__)
+        delete = Button(self.settings_tk, text="Delete", command=self.__delete__)
         delete.pack()
-        self.tk.mainloop()
+        self.settings_tk.mainloop()
 
     def __delete__(self):
         global xpos, actions
@@ -200,20 +182,33 @@ class Action:
         self.pos = 0
         canvas.delete("all")
         xpos -= 105
+        posx = 50
+        posy = 50
         actions.remove(self)
-        self.tk.destroy()
+        self.settings_tk.destroy()
         for x in range(len(actions)):
-            canvas.create_image(50 + 105 * x, ypos, image=(get_icon(actions[x].name + "_icon")))
+            if posx < 800:
+                canvas.create_image(posx, posy, image=actions[x].icon)
+                posx += 105
+            else:
+                posy += 105
+                posx = 50
+                canvas.create_image(posx, posy, image=actions[x].icon)
+                posx += 105
 
 
+# Mouse movement class
 class MouseMovement:
     def __init__(self):
         self.flag = False
 
     def mouse_pressed(self, event):
         global actions
+        y = 0
         for x in range(len(actions)):
-            if (x * 105 + 100) > event.x > (x * 105) and 250 < event.y < 350:
+            if x == 8 * (y + 1):
+                y += 1
+            if x * 105 + 100 - 840 * y > event.x > x * 105 - 840 * y and y * 105 < event.y < y * 105 + 100:
                 self.flag = True
                 actions[x].open_settings()
 
@@ -222,43 +217,39 @@ class MouseMovement:
             self.flag = False
 
 
+# Mouse movement class instantiation
 m = MouseMovement()
 
-canvas = Canvas(root, bg="#1F1F1F", width="830", height="685")
+# Root's canvas
+canvas = Canvas(root, bg="#1F1F1F", width="840", height="685")
 canvas.pack(side=RIGHT)
 canvas.bind('<ButtonPress-1>', m.mouse_pressed)
 canvas.bind('<ButtonRelease-1>', m.mouse_release)
 
+# Root's buttons
 go = Button(root, width=13, text='GO!', bg='black', fg='white', command=go)
 go.pack(side=TOP)
-
-ht = Button(root, command=ht)
-ht.config(image=ht_icon, width=pic_size, height=pic_size)
+ht = Button(root, command=lambda: actions.append(Action('Head Tilt', icons[0])), image=icons[0], width=pic_size,
+            height=pic_size)
 ht.pack(side=TOP)
-
-hr = Button(root, command=hr)
-hr.config(image=hr_icon, width=pic_size, height=pic_size)
+hr = Button(root, command=lambda: actions.append(Action('Head Rotate', icons[1])), image=icons[1], width=pic_size,
+            height=pic_size)
 hr.pack(side=TOP)
-
-run = Button(root, command=run)
-run.config(image=run_icon, width=pic_size, height=pic_size)
+run = Button(root, command=lambda: actions.append(Action('Move', icons[2])), image=icons[2], width=pic_size,
+             height=pic_size)
 run.pack(side=TOP)
-
-turn = Button(root, command=turn)
-turn.config(image=turn_icon, width=pic_size, height=pic_size)
+turn = Button(root, command=lambda: actions.append(Action('Turn', icons[3])), image=icons[3], width=pic_size,
+              height=pic_size)
 turn.pack(side=TOP)
-
-br = Button(root, command=br)
-br.config(image=br_icon, width=pic_size, height=pic_size)
+br = Button(root, command=lambda: actions.append(Action('Body Rotate', icons[4])), image=icons[4], width=pic_size,
+            height=pic_size)
 br.pack(side=TOP)
-
-wait = Button(root, command=wait)
-wait.config(image=wait_icon, width=pic_size, height=pic_size)
+wait = Button(root, command=lambda: actions.append(Action('Wait', icons[5])), image=icons[5], width=pic_size,
+              height=pic_size)
 wait.pack(side=TOP)
-
 del_all = Button(root, width=13, text='Delete All', bg='black', fg='white', command=del_all)
 del_all.pack(side=TOP)
 
+# Main Tk loop
 root.mainloop()
-
-os.system('xset r on')
+# os.system('xset r on')
